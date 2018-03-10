f5e5132d8704c94e5eccc4e569b53211450ed525
Michael Heuer <michael.heuer@rwth-aachen.de>
2018-03-10 23:31:33 +0100

    Added tests.
    Using the sphere_levedev_rule implementation by John Burkardt as a reference.

--- tests/ASphericalIntegrationTest.cpp
+++ tests/ASphericalIntegrationTest.cpp
@@ -1,16 +1,73 @@
 //
-// Created by dahl on 21.11.17.
+// Created by Heuer on 10.03.18.
 //
 
 #include <gtest/gtest.h>
 #include <Eigen/Core>
-
+#include "SphericalIntegration.h"
+#include "sphere_lebedev_rule.h"
 
 class ASphericalIntegrationTest : public ::testing::Test {
-
+public:
+    void SetUp() override {}
 };
 
-TEST_F(ASphericalIntegrationTest, CartesianGrid) {
 
-    ASSERT_TRUE(false);
+TEST_F(ASphericalIntegrationTest, JBurkardtReferenceCheck) {
+
+    for (const auto & rule : Lebedev::allRules) {
+        auto order = static_cast<unsigned>(rule);
+        auto calculated = Lebedev::getGrid(rule);
+
+        Eigen::MatrixX4d reference(order, 4);
+
+        double x[order], y[order], z[order], w[order];
+        ld_by_order(order, x, y, z, w);
+
+        auto xvec = Eigen::Map<Eigen::VectorXd>(x, order);
+        auto yvec = Eigen::Map<Eigen::VectorXd>(y, order);
+        auto zvec = Eigen::Map<Eigen::VectorXd>(z, order);
+        auto wvec = Eigen::Map<Eigen::VectorXd>(w, order);
+
+        reference.col(0) = xvec;
+        reference.col(1) = yvec;
+        reference.col(2) = zvec;
+        reference.col(3) = wvec;
+
+        ASSERT_TRUE(calculated.isApprox(reference,0.0));
+    }
 }
+
+TEST_F(ASphericalIntegrationTest, JBurkardtReferenceCheckElementWise) {
+
+    // check all rules
+    for (const auto & rule : Lebedev::allRules) {
+        auto order = static_cast<unsigned>(rule);
+        auto calculated = Lebedev::getGrid(rule);
+
+        Eigen::MatrixX4d reference(order, 4);
+
+        double x[order], y[order], z[order], w[order];
+        ld_by_order(order, x, y, z, w);
+        auto xvec = Eigen::Map<Eigen::VectorXd>(x, order);
+        auto yvec = Eigen::Map<Eigen::VectorXd>(y, order);
+        auto zvec = Eigen::Map<Eigen::VectorXd>(z, order);
+        auto wvec = Eigen::Map<Eigen::VectorXd>(w, order);
+
+        reference.col(0) = xvec;
+        reference.col(1) = yvec;
+        reference.col(2) = zvec;
+        reference.col(3) = wvec;
+
+
+        ASSERT_EQ(reference.rows(), calculated.rows());
+        ASSERT_EQ(reference.cols(), calculated.cols());
+
+        for (int i = 0; i < order; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                ASSERT_EQ(calculated.row(i)[j], reference.row(i)[j]);
+            }
+        }
+    }
+}
+
