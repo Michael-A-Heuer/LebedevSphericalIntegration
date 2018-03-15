49ef2d374483a71b8c6c15d6905ccf872ef779c4
Michael Heuer <michael.heuer@rwth-aachen.de>
2018-03-15 16:18:45 +0100

    Added new test employing a 3d Gauss function.

--- tests/ASphericalSurfaceIntegratorTest.cpp
+++ tests/ASphericalSurfaceIntegratorTest.cpp
@@ -12,6 +12,12 @@ public:
     };
 };
 
+class Gauss3d : public SpatialFunction{
+public:
+    double value(const Eigen::Vector3d &rvec) const override {
+        return std::exp(-rvec.squaredNorm());
+    };
+};
 
 class ASphericalSurfaceIntegratorTest : public ::testing::Test {
 public:
@@ -32,4 +38,32 @@ TEST_F(ASphericalSurfaceIntegratorTest, UnitSphereSurface) {
         double relError = std::abs(calculated - reference) / (reference);
         ASSERT_LE(relError, 2.2052e-14);
     }
+}
+
+TEST_F(ASphericalSurfaceIntegratorTest, GaussSurface) {
+    Gauss3d f;
+
+    //Check over a range of equally spaced radii over the interval [0,rEnd]
+    double rEnd = 5.0;
+    unsigned numberOfRadii = 10;
+
+    for (unsigned i = 0; i <= numberOfRadii; ++i) {
+
+        double r = rEnd * double(i)/double(numberOfRadii);
+        //Surface of the unit sphere
+
+        //analytical solution
+        double reference = 4.0*std::exp(-std::pow(r,2)) * M_PI * std::pow(r,2);
+
+        // Check all lebedev orders
+        for (const auto & order : Lebedev::allOrders) {
+            sphericalSurfaceIntegrator.changeGrid(order);
+            double calculated = sphericalSurfaceIntegrator.integrate(&f,r)* std::pow(r,2);
+
+            double absError = std::abs(calculated - reference);
+
+            ASSERT_NEAR(calculated,reference,1e-12); // low orders are expected to produce larger errors
+        }
+    }
+
 }
